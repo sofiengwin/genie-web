58de60fde5792b3285301589fbe0e30944d21a0a
andela-gogbara <godwin.onisofien@andela.com>
2016-09-06 22:25:06 +0100

    fix single token issue

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -5,13 +5,16 @@ import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
-export class ChatService {
+export class ChatService{
+  token: number
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) { 
+    this.token = Math.floor(Math.random() * 100000000)
+  }
 
   sendToGenie(message: string): Observable<any> {
     let url = "https://genie-api.eu-gb.mybluemix.net/api/v1/conversations/web_mobile"
-    let body = JSON.stringify({ message: message, token: "testtoken" });
+    let body = JSON.stringify({ message: message, token: this.token });
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
@@ -27,15 +30,3 @@ export class ChatService {
     return Observable.throw(error.json().error || "Server Error"); 
   }
 }
-
-// export const CHATS: Ichat[] = [
-//   {  message: "Genie first message", messageAuthor: "genie" },
-//   {  message: "Genie first message", messageAuthor: "user" },
-//   {  message: "Genie first message", messageAuthor: "genie" },
-//   {  message: "Genie first message", messageAuthor: "user" },
-//   {  message: "Genie first message", messageAuthor: "genie" },
-//   {  message: "Genie first message", messageAuthor: "user" },
-//   {  message: "Genie first message", messageAuthor: "genie" },
-//   {  message: "Genie first message", messageAuthor: "user" },
-//   {  message: "Genie first message", messageAuthor: "genie" },
-// ]
